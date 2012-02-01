925a984a579540eac0d4f32f7160bc482b08690c
bmdelaune@gmail.com <bmdelaune@gmail.com@0fe21ba0-e667-ccad-d106-3b8073cba30e>
2012-02-01 15:33:18 +0000

    added Credit Application Project

---
+++ Group5_Project1_CreditApplication/Group5_Project1_CreditApplication/stdafx.cpp
@@ -0,0 +1,7 @@
+// stdafx.cpp : source file that includes just the standard includes
+// Group5_Project1_CreditApplication.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+
